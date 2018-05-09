fa4ba4325b72cec19f01ca451572d6597342e027
04421368202 <paula.kethleen@gmail.com>
2018-05-09 11:05:36 -0400

    gravar com o banco de dados parte 1

--- Paula/src/br/edu/ifro/AlunoController.java
+++ Paula/src/br/edu/ifro/AlunoController.java
@@ -5,9 +5,16 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.modelo.Aluno;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -16,6 +23,9 @@ import javafx.fxml.Initializable;
  */
 public class AlunoController implements Initializable {
 
+    @FXML
+    private JFXTextField txtNome;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +33,24 @@ public class AlunoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void salvar(ActionEvent event) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Aula");
+         EntityManager em = emf.createEntityManager();
+         
+         Aluno aluno1 = new Aluno  ();
+         aluno1.setNome(txtNome.getText());
+         
+         em.getTransaction().begin();
+         
+         em.getTransaction().commit();
+         
+         
+    }
+
+    @FXML
+    private void fechar(ActionEvent event) {
+    }
     
 }
